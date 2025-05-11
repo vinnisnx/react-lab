538b6ca3176a1d431a8620f2c65545d5825aea95
student <ss@s>
2025-05-11 09:35:46 +0200

    Login logic transferred to separate module

--- src/App.js
+++ src/App.js
@@ -2,56 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import LoginForm from "./LoginForm";
+
 
 function App() {
-    const [email, setEmail] = useState('typehere@example.com');
-    const [text_box, setBox] = useState(<input type="text" onChange={handleChange}/>);
-    const [button, setButton] = useState(<button type="button" onClick={login}>LogIn</button>);
-    const [instruction, setInstruction] = useState(<div>Wpisz swoj email żeby zalogować</div>);
-    const [refreshPage, setRefresher] = useState(<button hidden type="button" style={{display:"none"}} onClick={logout}>LogOut</button>);
-    const [flag, setFlag] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(null);
 
-    function handleChange(event) {
-        setEmail(event.target.value);
+    function login(username) {
+        if (username) {
+            setLoggedIn(username)
+        }
     }
-
-    function login() {
-        setFlag(true)
-        setInstruction(<div hidden>Wpisz swoj email żeby zalogować</div>)
-        setBox(<input hidden type="text" onChange={handleChange}/>)
-        setButton(<button type="button" style={{display:"none"}} onClick={login}>LogIn</button>)
-        setRefresher(<button type="button" onClick={logout}>LogOut</button>)
-    }
-
     function logout() {
-        setFlag(false)
-        setInstruction(<div>Wpisz swoj email żeby zalogować</div>)
-        setBox(<input type="text" onChange={handleChange}/>)
-        setButton(<button type="button" onClick={login}>LogIn</button>)
-        setRefresher(<button type="button" style={{display:"none"}} onClick={logout}>LogOut</button>)
+        setLoggedIn(null);
     }
 
-    let message = '';
-    if (email.length < 10) {
-        message = 'Email jest którki';
-    } else if (email.length < 20) {
-        message = 'Jest OK';
+    let content;
+    if (loggedIn) {
+        content = <div>
+            <h2> Witaj {loggedIn}</h2>
+            <button onClick={logout}>Wyloguj</button>
+        </div>
     } else {
-        message = 'Email za długi'
+        content = <LoginForm onLogin={(username) => login(username)}/>;
     }
+
 
     return (
-        <div className="App">
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twoj email {email}</h2>
-            {instruction}
-            <div>{message}</div>
-            {flag && (
-                <div>Welcome {email}</div>
-            )}
-            {text_box}
-            {button}
-            {refreshPage}
+        <div >
+            <h1>System do zapisów</h1>
+            {content}
 
         </div>
     );
